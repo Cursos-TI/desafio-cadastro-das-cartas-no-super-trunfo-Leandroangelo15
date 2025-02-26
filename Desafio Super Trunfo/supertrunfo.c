657f8db284f159db159774bd7176e65c14af5b6e
Leandroangelo15 <leandroangelo2024@hotmail.com>
2025-02-26 15:56:17 +0000

    novoooo commit

--- Desafio Super Trunfo/supertrunfo.c
+++ Desafio Super Trunfo/supertrunfo.c
@@ -1,59 +1,64 @@
 #include <stdio.h>
 
-int main(){
+    int main(){
 
-    char estado1[50], estado2[50];
-    char Cidade1[20], Cidade2[20];
-    char codigo1, codigo2;
-    int populacao1, populacao2;
-    float areakm1, areakm2;
-    int pib1, pib2;
-    int turisticos1, turistico2;
+        //Dados para cadastro carta 01
 
-    printf("Cadrasto da Carta1: \n");
+    char estado1 [50], codigocarta1 [50], nomedacidade1 [50];
 
-    printf("Digite o nome do estado: \n");
-    scanf("%s", &estado1);
+    int populacao1, numeropontosturistico1;
+
+    float areaemkm1, pib1;
+
+    //solcitando dados para cadastro carta 01,
+
+    printf("Digite o nome do Estado: \n");
+    scanf("%s", estado1);
 
     printf("Digite nome da Cidade: \n");
-    scanf("%s", &Cidade1);
+    scanf("%s", nomedacidade1);
+
+    printf("Digite numero da populacao1: \n");
+    scanf("%d", populacao1);
 
     printf("Digite o nome do codigo1: \n");
-    scanf("%s", &codigo1);
-
-    printf("Digite o nome do populacao1: \n");
-    scanf("%d", &populacao1);
+    scanf("%s", codigocarta1);
 
     printf("Digite o nome do areakm1: \n");
-    scanf("%f", &areakm1);
+    scanf("%f", areaemkm1);
 
     printf("Digite o nome do pib1: \n");
-    scanf("%f", &pib1);
+    scanf("%f", pib1);
 
     printf("Digite o nome do turisticos1: \n");
-    scanf("%e", &turisticos1);
+    scanf("%e", numeropontosturistico1);
 
-    printf("Cadrasto da Carta2: \n");
+    //Dados para cadastro carta 02
 
+    char estado2 [50], codigocarta2 [50], nomedacidade2 [50];
+
+   int populacao2, numeropontosturistico2;
+
+    float areaemkm2, pib2;
     
     printf("Digite o nome do estado2: \n");
-    scanf ("%s", &estado2);
+    scanf ("%s", estado2);
 
     printf("Digite nome da Cidade2: \n");
-    scanf ("%s", &Cidade2);
+    scanf ("%s", nomedacidade2);
 
     printf("Digite o nome do codigo2: \n");
-    scanf("%s", &codigo2);
+    scanf("%s", codigocarta2);
 
-    printf("Digite o nome do populacao2: \n");
-    scanf("%d", &populacao2);
+    printf("Digite numero da  populacao2: \n");
+    scanf("%d", populacao2);
 
     printf("Digite o nome do areakm2: \n");
-    scanf("%f", &areakm2);
+    scanf("%f", areaemkm2);
 
     printf("Digite o nome do pib2: \n");
-    scanf("%f", &pib2);
+    scanf("%f", pib2);
 
     printf("Digite o nome do turisticos2: \n");
-    scanf("%e", &turistico2);
+    scanf("%e", numeropontosturistico2);
 }
